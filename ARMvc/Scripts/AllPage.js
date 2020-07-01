eb88f43390e8fb2e4d97ecf39a75f17755be1008
JLTravels <lijomonalappuzha@gmail.com>
2020-07-02 02:38:02 +0530

    Responsive

--- ARMvc/Scripts/AllPage.js
+++ ARMvc/Scripts/AllPage.js
@@ -8,3 +8,20 @@ function fnHideLoader() {
         $('div.overlay').hide();
     }, 500);
 }
+
+$(document).ready(function () {
+    //Mobile Menu
+    $('.MobTopMenu').click(function () {
+        if (!$('html').hasClass('show_menu')) {
+            $('html').addClass('show_menu');
+        } else {
+            $('html').removeClass('show_menu');
+        }
+    });
+
+    $('.MenuOverlay').click(function () {
+        if ($('html').hasClass('show_menu')) {
+            $('html').removeClass('show_menu');
+        }
+    });
+});
